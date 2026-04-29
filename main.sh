1bb72f24d7c43e606584786438a16cdd62777811
luffy <iitbombay2702@gmail.com>
2026-04-29 09:34:09 +0530

    user authentication and registration process

--- main.sh
+++ main.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
-for ((x=1;x<=2;x++)); do
-	read -p "Player$x, Enter your username:" username
-        if [[ -z $(grep "^$username"$'\t' users.tsv) ]]; then
-		echo "Username does not exist, do you want to register:[Y/N]"
-		read response
-                if [[ $response == "Y" ]]; then
-			read -p "Make your password:" password
-			echo -e "$username\t$(echo $password | sha256sum )" >> users.tsv
-                fi
-        else 
-		while true; do
-			read -p "Player$x, Enter your password:" password
-			if [[  $(grep "^$username"$'\t' users.tsv | cut -d '$t' -f 2) != $(echo $password | sha256sum) ]]; then
-				echo "Wrong password, Please try again"
-		                continue
-		        else 
-			   break
-		        fi
-	        done	
+
+USER_FILE="users.tsv"
+
+# create file if not exists
+touch $USER_FILE
+
+hash_password() {
+    echo -n "$1" | sha256sum | awk '{print $1}'
+}
+
+authenticate_user() {
+    while true; do
+        read -p "Enter username: " username
+        read -s -p "Enter password: " password
+        echo ""
+
+        hash=$(hash_password "$password")
+
+        if grep -q "^$username" "$USER_FILE"; then
+            stored_hash=$(grep "^$username" "$USER_FILE" | cut -f2)
+
+            if [ "$hash" == "$stored_hash" ]; then
+                echo "Login successful!" >&2   
+                echo "$username"
+                return
+            else
+                echo "Incorrect password. Try again." >&2
+            fi
+
+        else
+            read -p "User not found. Register? (y/n): " choice
+
+            if [ "$choice" == "y" ]; then
+                echo -e "$username\t$hash" >> "$USER_FILE"
+                echo "User registered!" >&2
+                echo "$username"
+                return
+            fi
         fi
+    done
+}
+
+echo "===== Player 1 Login ====="
+user1=$(authenticate_user)
+
+echo "===== Player 2 Login ====="
+while true; do
+    user2=$(authenticate_user)
+
+    if [ "$user1" != "$user2" ]; then
+        break
+    else
+        echo "Players must be different!"
+    fi
 done
-echo "Both players Authenticated"
+
+ # launch game i used in my pc for git bash running 
+# "/c/Users/iitbo/AppData/Local/Programs/Python/Python310/python" game.py "$user1" "$user2"
